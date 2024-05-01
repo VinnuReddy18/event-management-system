32c2d7279266bcaa6700e05b1ecf1e7665b3895b
Manjari Rathore <manjarirathore1512@gmail.com>
2024-05-01 16:45:36 +0530

    created dtos

--- src/main/java/org/example/eventmanagementsystem/dtos/VenueDto.java
+++ src/main/java/org/example/eventmanagementsystem/dtos/VenueDto.java
@@ -1,4 +1,19 @@
 package org.example.eventmanagementsystem.dtos;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import org.example.eventmanagementsystem.models.Event;
+import org.example.eventmanagementsystem.models.enums.EVenueAvailability;
+
+import java.util.List;
+
 public class VenueDto {
+    private Long capacity;
+    private String venueName;
+
+    private List<Event> events;
+    private String venueLocation;
+    private EVenueAvailability venueAvailability ;
 }
